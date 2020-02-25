68d78c7f86610ad9a6cecba2bd674b0276ee5197
shubham <er.sjain28@gmail.com>
2020-02-25 23:49:08 +0530

    Add Redux functionality using context and reducer

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,6 +5,7 @@ const modalRoot = document.getElementById("modal");
 
 const Modal = ({ children }) => {
   const elRef = useRef(null);
+  
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
@@ -14,7 +15,7 @@ const Modal = ({ children }) => {
     return () => modalRoot.removeChild(elRef.current);
   }, []);
 
-  return createPortal(<div>{children}</div>, elRef.current);
+  return createPortal(<div>{children}</div>, modalRoot);
 };
 
 export default Modal;
